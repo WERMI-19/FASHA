0c8b5da86e1ee3c8268354abce70742de9c4a7b7
WERMI <adamalivres19@gmail.com>
2025-09-16 07:48:27 +0100

    + RefreshApex sur LWC =>Maj Dynamique

--- force-app/main/default/lwc/accountOrdersSummary/accountOrdersSummary.js
+++ force-app/main/default/lwc/accountOrdersSummary/accountOrdersSummary.js
@@ -1,4 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
+// Étape 1: Importer refreshApex
+import { refreshApex } from '@salesforce/apex';
 import getSumOrdersByAccount from '@salesforce/apex/OrdersController.getSumOrdersByAccount';
 
 export default class AccountOrdersSummary extends LightningElement {
@@ -6,19 +8,28 @@ export default class AccountOrdersSummary extends LightningElement {
     @api recordId;
     sumOrdersOfCurrentAccount;
     error;
-    
-    // Utilisation de @wire pour appeler la méthode Apex de manière réactive
+    wiredSumOrdersResult;
+
     @wire(getSumOrdersByAccount, { accountId: '$recordId' })
-    wiredSumOrders({ error, data }) {
-        if (data) {
-            this.sumOrdersOfCurrentAccount = data;
+    wiredSumOrders(result) {
+        // On stocke le résultat complet du @wire
+        this.wiredSumOrdersResult = result;
+        if (result.data) {
+            this.sumOrdersOfCurrentAccount = result.data;
             this.error = undefined;
-        } else if (error) {
-            this.error = error;
+        } else if (result.error) {
+            this.error = result.error;
             this.sumOrdersOfCurrentAccount = undefined;
         }
     }
+    
     get hasPositiveAmount() {
         return this.sumOrdersOfCurrentAccount && this.sumOrdersOfCurrentAccount > 0;
+    }
+
+    //  fonction pour gérer le clic sur le bouton
+    handleRefresh() {
+        // Appelle refreshApex pour rafraîchir les données du @wire
+        return refreshApex(this.wiredSumOrdersResult);
     }
 }
